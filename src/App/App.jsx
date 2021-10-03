48e8e2fa72ba1615801de9e85bf736473cca2264
Ryan Kyser <ryankyser@gmail.com>
2021-10-02 21:10:41 -0400

    Add KeyboardModeDropDown

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -2,13 +2,19 @@ import './App.css';
 import React from 'react';
 import Keyboard from '../Features/Keyboard/Keyboard';
 import OctaveDropDown from '../Features/Keyboard/OctaveDropDown';
+import KeyboardModeDropDown from '../Features/Keyboard/KeyboardModeDropDown';
 
 const App = () => (
-  <div className="App">
-    <h1>Keyboard</h1>
-    <Keyboard />
-    <h2>Options</h2>
-    <OctaveDropDown />
+  <div className="with-sidebar">
+    <div>
+      <h2>Options</h2>
+      <OctaveDropDown />
+      <KeyboardModeDropDown />
+    </div>
+    <div>
+      <h1>Keyboard</h1>
+      <Keyboard />
+    </div>
   </div>
 );
 
