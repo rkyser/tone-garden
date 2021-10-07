9edd7ceb22c6f37478c726bbc53eac3b280e4184
Ryan Kyser <ryankyser@gmail.com>
2021-10-06 23:05:35 -0400

    Add SVG-based keyboard layout

--- src/Features/Keyboard/KeyboardKey.jsx
+++ src/Features/Keyboard/KeyboardKey.jsx
@@ -2,17 +2,46 @@ import './KeyboardKey.css';
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const KeyboardKey = ({ keyName, note, isDown }) => (
-  <div className={`keyboard-key ${isDown ? 'active' : ''}`}>
-    {keyName}
-    -
-    {note}
-  </div>
-);
+const KeyboardKey = ({
+  x,
+  y,
+  width,
+  height,
+  keyName,
+  note,
+  isDown,
+}) => {
+  const noteTextElement = note !== ''
+    ? (<text x={x + (width / 2)} y={y + (height / 2)} dominantBaseline="middle" textAnchor="middle">{note}</text>)
+    : '';
+
+  return (
+    <g>
+      <rect
+        x={x}
+        y={y}
+        width={width}
+        height={height}
+        rx={5}
+        className={`keyboard-key ${isDown ? 'active' : ''}`}
+      />
+      <text x={x + 10} y={y + 15} dominantBaseline="middle" textAnchor="left" className="keyboard-key-label">{keyName}</text>
+      {noteTextElement}
+    </g>
+  );
+};
+
+KeyboardKey.defaultProps = {
+  note: '',
+};
 
 KeyboardKey.propTypes = {
+  x: PropTypes.number.isRequired,
+  y: PropTypes.number.isRequired,
+  width: PropTypes.number.isRequired,
+  height: PropTypes.number.isRequired,
   keyName: PropTypes.string.isRequired,
-  note: PropTypes.string.isRequired,
+  note: PropTypes.string,
   isDown: PropTypes.bool.isRequired,
 };
 
